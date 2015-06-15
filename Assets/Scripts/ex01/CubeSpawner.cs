9f130f2f1a849359b81bd010b59f8353d0b32840
Ahmed EL-KADHI <ael-kadh@e2r2p5.42.fr>
2015-06-15 16:20:10 +0200

    day0

---
+++ Assets/Scripts/ex01/CubeSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawner : MonoBehaviour {
+
+	#region Attributes
+	[SerializeField] private GameObject[] KeyLine;
+	[SerializeField] private GameObject[] keys;
+	[SerializeField] private GameObject LineDown;
+
+	private bool[] isKey = new bool[]{false, false, false};
+	private KeyCode[] Key_Code = new KeyCode[]{KeyCode.A, KeyCode.S, KeyCode.D};
+	private GameObject[] Boxes = new GameObject[]{null, null, null};
+	#endregion
+
+	private GameObject CreateBox (GameObject box, Transform line, int choose) {
+		GameObject obj = Instantiate (box, new Vector3(line.position.x, transform.position.y, 0), box.transform.rotation) as GameObject;
+		Cube cube = obj.AddComponent<Cube> ();
+		cube.LineDown = LineDown;
+		cube.key = Key_Code [choose];
+		return obj;
+	}
+
+	void Update() {
+		int choose = Random.Range (0, 3);
+		if (!isKey[choose])
+		{
+			Boxes[choose] = CreateBox(keys[choose], KeyLine[choose].transform, choose);
+			isKey[choose] = true;
+		}
+		for (int i=0; i<3; i++)
+		{
+			if (Boxes[i] == null && isKey[i])
+				isKey[i] = false;
+		}
+	}
+}
---
+++ Assets/Scripts/ex01/cube.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	private float speed;
+	private bool click = false;
+
+	public GameObject LineDown;
+	public KeyCode key;
+
+	void Start () {
+		speed = Random.Range (0.4f, 1.5f);
+	}
+	
+	void Update () {
+		transform.Translate(Vector3.down * speed * Time.deltaTime);
+		if (transform.position.y <= LineDown.transform.position.y || (click = Input.GetKeyDown(key)))
+		{
+			if (click)
+				Debug.Log("Precision: " + (transform.position.y - LineDown.transform.position.y));
+			Destroy(gameObject);
+		}
+	}
+}
